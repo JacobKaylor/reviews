3c1b92275afbe424d8484aa894c2cf5105559c3b
JacobKaylor <jkaylor20@yahoo.com>
2018-02-16 10:42:44 -0500

    Finding a second review

--- src/test/java/org/wecancodeit/reviews/ReviewRepositoryTest.java
+++ src/test/java/org/wecancodeit/reviews/ReviewRepositoryTest.java
@@ -6,17 +6,29 @@ import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
 public class ReviewRepositoryTest {
-	
+
 	private ReviewRepository underTest;
-	
+
 	private long reviewId = 233;
-	private Review firstReview = new Review(reviewId, "Pink Floyd Vinyl","", "Music", "Pink Floyd The Wall Vinyl Record");
-	
+	private Review firstReview = new Review(reviewId, "Pink Floyd Vinyl", "", "Music",
+			"Pink Floyd The Wall Vinyl Record");
+
+	private long secondReviewId = 233;
+	private Review secondReview = new Review(secondReviewId, "Lumineers Vinyl", "", "Music",
+			"Lumineers Cleopatra Vinyl Record");
+
 	@Test
 	public void shouldFindFirstReview() {
 		underTest = new ReviewRepository(firstReview);
 		Review result = underTest.findReview(reviewId);
 		assertThat(result, is(firstReview));
+	}
+
+	@Test
+	public void shouldFindSecondReview() {
+		underTest = new ReviewRepository(firstReview, secondReview);
+		Review result = underTest.findReview(secondReviewId);
+		assertThat(result, is(secondReview));
 	}
 
 }
