29879bd37ec52c07db5f80bb0686846ba1b0e86e
JacobKaylor <jkaylor20@yahoo.com>
2018-02-16 11:25:46 -0500

    added getters in Review class

--- src/main/java/org/wecancodeit/reviews/Review.java
+++ src/main/java/org/wecancodeit/reviews/Review.java
@@ -1,7 +1,7 @@
 package org.wecancodeit.reviews;
 
 public class Review {
-	
+
 	public Review(long reviewId, String title, String imageUrl, String category, String content) {
 		this.reviewId = reviewId;
 		this.title = title;
@@ -9,19 +9,29 @@ public class Review {
 		this.category = category;
 		this.content = content;
 	}
+
 	private long reviewId;
 	private String title;
 	private String imageUrl;
 	private String category;
 	private String content;
-	
-	
+
 	public Long getId() {
-		
 		return reviewId;
 	}
 
+	public String getTitle() {
+		return title;
+	}
 	
-	
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getContent() {
+		return content;
+	}
 
 }
