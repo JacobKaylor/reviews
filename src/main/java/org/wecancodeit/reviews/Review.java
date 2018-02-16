ae9c3552f3f71b97809d364888ec813a76ea802a
JacobKaylor <jkaylor20@yahoo.com>
2018-02-16 10:05:34 -0500

    first commit

---
+++ src/main/java/org/wecancodeit/reviews/Review.java
@@ -0,0 +1,14 @@
+package org.wecancodeit.reviews;
+
+public class Review {
+	
+	private long reviewId;
+	private String title;
+	private String imageUrl;
+	private String category;
+	private String content;
+
+	
+	
+
+}
---
+++ src/test/java/org/wecancodeit/reviews/ReviewRepositoryTest.java
@@ -0,0 +1,10 @@
+package org.wecancodeit.reviews;
+
+public class ReviewRepositoryTest {
+	
+	private ReviewRepository underTest;
+	
+	private long reviewId = 233;
+	private Review firstReview = new Review(reviewId, "Pink Floyd Vinyl", "Music", "Pink Floyd The Wall Vinyl Record");
+
+}
